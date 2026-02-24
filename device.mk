1bca372e456ac204e82ebd386d31f3650a020a74
UsrBinLuna <matteo111209@gmail.com>
2026-02-24 11:36:16 -0300

    so uhh kind of works now

--- device.mk
+++ device.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Use direct paths instead of variables to ensure resolution
+$(call inherit-product, build/make/target/product/handheld_system.mk)
+$(call inherit-product, build/make/target/product/handheld_vendor.mk)
+$(call inherit-product, build/make/target/product/languages_full.mk)
+
+DEVICE_PATH := device/lenovo/achilles6_row_wifi
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
@@ -19,29 +26,29 @@ PRODUCT_CHARACTERISTICS := tablet
 # Rootdir
 PRODUCT_PACKAGES += \
 
-PRODUCT_PACKAGES += \
-    fstab.enableswap \
-    init.connectivity.rc \
-    meta_init.rc \
-    factory_init.connectivity.rc \
-    init.modem.rc \
-    init.ago.rc \
-    factory_init.rc \
-    init.aee.rc \
-    multi_init.rc \
-    meta_init.modem.rc \
-    init.mt8768.rc \
-    factory_init.project.rc \
-    init.mt6765.usb.rc \
-    init.project.rc \
-    init.sensor_1_0.rc \
-    meta_init.project.rc \
-    meta_init.connectivity.rc \
-    init.mt6765.rc \
-    ueventd.rc \
-    init.recovery.mt8768.rc \
-    init.recovery.mt6765.rc \
-    init.rc \
+#PRODUCT_PACKAGES += \
+#    fstab.enableswap \
+#    init.connectivity.rc \
+#meta_init.rc \
+#    factory_init.connectivity.rc \
+#    init.modem.rc \
+#    init.ago.rc \
+#    factory_init.rc \
+#    init.aee.rc \
+#    multi_init.rc \
+#    meta_init.modem.rc \
+#    init.mt8768.rc \
+#    factory_init.project.rc \
+#    init.mt6765.usb.rc \
+#    init.project.rc \
+#    init.sensor_1_0.rc \
+#    meta_init.project.rc \
+#    meta_init.connectivity.rc \
+#    init.mt6765.rc \
+#    ueventd.rc \
+#    init.recovery.mt8768.rc \
+#    init.recovery.mt6765.rc \
+#    init.rc \
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
@@ -53,7 +60,31 @@ PRODUCT_SOONG_NAMESPACES += \
 # Audio policy configuration
 USE_XML_AUDIO_POLICY_CONF := 1
 
+# Copy fstab and init scripts to the ramdisk
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/fstab.mt6765:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6765 \
+    $(DEVICE_PATH)/rootdir/etc/fstab.mt8768:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt8768 \
+    $(DEVICE_PATH)/rootdir/etc/init.mt6765.rc:$(TARGET_COPY_OUT_RAMDISK)/init.mt6765.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.mt6765.usb.rc:$(TARGET_COPY_OUT_RAMDISK)/init.mt6765.usb.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.mt8768.rc:$(TARGET_COPY_OUT_RAMDISK)/init.mt8768.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.project.rc:$(TARGET_COPY_OUT_RAMDISK)/init.project.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.connectivity.rc:$(TARGET_COPY_OUT_RAMDISK)/init.connectivity.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.sensor_1_0.rc:$(TARGET_COPY_OUT_RAMDISK)/init.sensor_1_0.rc \
+    $(DEVICE_PATH)/rootdir/etc/ueventd.rc:root/ueventd.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.rc:root/init.rc \
+    $(DEVICE_PATH)/rootdir/etc/fstab.mt6765:root/fstab.mt6765
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/fstab.mt6765:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mt6765 \
+    $(DEVICE_PATH)/rootdir/etc/init.mt6765.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.mt6765.usb.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.project.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.project.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.sensor_1_0.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.sensor_1_0.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.connectivity.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.connectivity.rc \
+    $(DEVICE_PATH)/rootdir/etc/init.ago.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.ago.rc
+
+# recovery
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/init.recovery.mt6765.rc:root/init.recovery.mt6765.rc
 
 # Inherit the proprietary files
 # $(call inherit-product, vendor/lenovo/achilles6_row_wifi/achilles6_row_wifi-vendor.mk)
--- lineage_achilles6_row_wifi.mk
+++ lineage_achilles6_row_wifi.mk
@@ -5,20 +5,19 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Inherit from achilles6_row_wifi device
-$(call inherit-product, device/lenovo/achilles6_row_wifi/device.mk)
 
 PRODUCT_DEVICE := achilles6_row_wifi
 PRODUCT_NAME := lineage_achilles6_row_wifi
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo TB-X606F
 PRODUCT_MANUFACTURER := lenovo
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit from achilles6_row_wifi device
+$(call inherit-product, device/lenovo/achilles6_row_wifi/device.mk)
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
